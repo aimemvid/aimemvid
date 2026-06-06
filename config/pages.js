3acb475f49b1ae9494c5b07167ab8dc7928110d9
aimemvid <aimemvid@gmail.com>
2026-06-07 02:56:06 +0700

    Update pages.js

--- config/pages.js
+++ config/pages.js
@@ -1,17 +1,7 @@
-// config/pages.js
+import { tentangKami } from "./pages/tentang-kami.js";
+import { kontak } from "./pages/kontak.js";
 
 export const PAGES = {
-  "/tentang-kami": {
-    title: "Tentang Kami",
-    desc: "Profil perusahaan",
-    content: "<h1>Tentang Kami</h1>"
-  },
-
-  "/kontak": {
-    title: "Kontak",
-    desc: "Hubungi kami",
-    canonical: "/kontak",
-    image: "",
-    content: "<h1>Hubungi kami</h1>"
-  }
+  "/tentang-kami": tentangKami,
+  "/kontak": kontak
 };
