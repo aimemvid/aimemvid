244bb1e813afd326ca7fba650f600cf257361921
aimemvid <aimemvid@gmail.com>
2026-06-07 00:43:13 +0700

    Update normal.js

--- templates/normal.js
+++ templates/normal.js
@@ -4,8 +4,8 @@ import { CONFIG } from "../config/site.js";
 
 import { head } from "./parts/head.js";
 import { header } from "./parts/header.js";
-import { secheader } from "./parts/section-header.js";
-import { secfooter } from "./parts/section-footer.js";
+import { secheader } from "./parts/secheader.js";
+import { secfooter } from "./parts/secfooter.js";
 import { footer } from "./parts/footer.js";
 
 export function normalTemplate() {
