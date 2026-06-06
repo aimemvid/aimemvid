a9f4664b5b9f5be0a371e4a1d7d3d6637bbccb0e
aimemvid <aimemvid@gmail.com>
2026-06-07 00:22:56 +0700

    Update rss.js

--- functions/rss.js
+++ functions/rss.js
@@ -1,1 +1,49 @@
+// functions/rss.js
 
+import { CONFIG } from "../config/site.js";
+
+export function rssPage() {
+
+  const now = new Date().toUTCString();
+
+  return new Response(
+`<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0">
+
+<channel>
+
+<title>${CONFIG.site_name}</title>
+
+<link>${CONFIG.canonical}</link>
+
+<description>${CONFIG.desc}</description>
+
+<language>id-ID</language>
+
+<item>
+  <title>${CONFIG.title}</title>
+  <link>${CONFIG.canonical}</link>
+  <guid>${CONFIG.canonical}</guid>
+  <pubDate>${now}</pubDate>
+  <description><![CDATA[${CONFIG.desc}]]></description>
+</item>
+
+<item>
+  <title>${CONFIG.title} AMP</title>
+  <link>${CONFIG.canonical}amp</link>
+  <guid>${CONFIG.canonical}amp</guid>
+  <pubDate>${now}</pubDate>
+  <description><![CDATA[${CONFIG.desc}]]></description>
+</item>
+
+</channel>
+
+</rss>`,
+    {
+      headers: {
+        "content-type": "application/rss+xml; charset=UTF-8",
+        "cache-control": "public, max-age=86400"
+      }
+    }
+  );
+}
