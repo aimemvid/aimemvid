bf91686b9a82a59f3347c0c3021a5989b30f0a74
aimemvid <aimemvid@gmail.com>
2026-06-07 00:26:32 +0700

    Create [[path]].js

---
+++ functions/[[path]].js
@@ -0,0 +1,39 @@
+// functions/[[path]].js
+
+import { homePage } from "./home.js";
+import { ampPage } from "./amp.js";
+import { robotsPage } from "./robots.js";
+import { sitemapPage } from "./sitemap.js";
+import { rssPage } from "./rss.js";
+
+export async function onRequest({ request }) {
+
+  const { pathname } = new URL(request.url);
+
+  if (pathname === "/") {
+    return homePage(request);
+  }
+
+  if (pathname === "/amp") {
+    return ampPage(request);
+  }
+
+  if (pathname === "/robots.txt") {
+    return robotsPage();
+  }
+
+  if (pathname === "/sitemap.xml") {
+    return sitemapPage();
+  }
+
+  if (pathname === "/rss.xml") {
+    return rssPage();
+  }
+
+  return new Response("404 - Not Found", {
+    status: 404,
+    headers: {
+      "content-type": "text/plain; charset=UTF-8"
+    }
+  });
+}
