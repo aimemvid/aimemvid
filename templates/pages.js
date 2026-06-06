4f75bbfdd8b8b2d1f0d318d24d15c9f94b07bba9
aimemvid <aimemvid@gmail.com>
2026-06-07 02:29:44 +0700

    Create pages.js

---
+++ templates/pages.js
@@ -0,0 +1,15 @@
+export function pagesTemplate(page) {
+  return `<!DOCTYPE html>
+<html lang="id">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<title>${page.title}</title>
+</head>
+<body>
+
+${page.content}
+
+</body>
+</html>`;
+}
