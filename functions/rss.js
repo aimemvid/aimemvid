2ac389aaab373e12dee5f9d8d07607ce7b5394da
aimemvid <aimemvid@gmail.com>
2026-06-07 02:46:22 +0700

    Update rss.js

--- functions/rss.js
+++ functions/rss.js
@@ -1,13 +1,22 @@
 // functions/rss.js
 
 import { CONFIG } from "../config/site.js";
+import { PAGES } from "../config/pages.js";
 
 export function rssPage() {
 
-  const now = new Date().toUTCString();
+  const items = Object.entries(PAGES)
+    .map(([path, page]) => `
+<item>
+  <title><![CDATA[${page.title}]]></title>
+  <link>${CONFIG.canonical.replace(/\/$/, "")}${path}</link>
+  <guid>${CONFIG.canonical.replace(/\/$/, "")}${path}</guid>
+  <description><![CDATA[${page.desc || ""}]]></description>
+  <pubDate>${new Date().toUTCString()}</pubDate>
+</item>`)
+    .join("");
 
-  return new Response(
-`<?xml version="1.0" encoding="UTF-8"?>
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0">
 
 <channel>
@@ -21,29 +30,24 @@ export function rssPage() {
 <language>id-ID</language>
 
 <item>
-  <title>${CONFIG.title}</title>
+  <title><![CDATA[${CONFIG.title}]]></title>
   <link>${CONFIG.canonical}</link>
   <guid>${CONFIG.canonical}</guid>
-  <pubDate>${now}</pubDate>
   <description><![CDATA[${CONFIG.desc}]]></description>
+  <pubDate>${new Date().toUTCString()}</pubDate>
 </item>
 
-<item>
-  <title>${CONFIG.title} AMP</title>
-  <link>${CONFIG.canonical}amp</link>
-  <guid>${CONFIG.canonical}amp</guid>
-  <pubDate>${now}</pubDate>
-  <description><![CDATA[${CONFIG.desc}]]></description>
-</item>
+${items}
 
 </channel>
 
-</rss>`,
-    {
-      headers: {
-        "content-type": "application/rss+xml; charset=UTF-8",
-        "cache-control": "public, max-age=86400"
-      }
+</rss>`;
+
+  return new Response(xml, {
+    status: 200,
+    headers: {
+      "content-type": "application/rss+xml; charset=UTF-8",
+      "cache-control": "public, max-age=86400"
     }
-  );
+  });
 }
