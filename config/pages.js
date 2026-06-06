9f6bcf9331d7c503e3594080623b4362b0e79821
aimemvid <aimemvid@gmail.com>
2026-06-07 01:01:28 +0700

    Create pages.js

---
+++ config/pages.js
@@ -0,0 +1,11 @@
+export const PAGES = {
+  "/tentang-kami": {
+    title: "Tentang Kami",
+    content: "<h1>Tentang Kami</h1>"
+  },
+
+  "/kontak": {
+    title: "Kontak",
+    content: "<h1>Kontak</h1>"
+  }
+};
